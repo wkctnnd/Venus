6c097e8fee430176cf3f595ee33ed748d1fe4d5a
wangkaichen <wkctnnd@sina.com>
2015-01-09 18:05:46 +0800

    i am learning to use git and cmake

---
+++ build/CMakeFiles/Venus.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Venus.dir/main.obj"
+  "Venus.pdb"
+  "Venus.exe"
+  "Venus.exe.manifest"
+  "Venus.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Venus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
