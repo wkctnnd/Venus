b7d918c42c902a568eb8af882101383c0f2d6659
wangkaichen <wkctnnd@sina.com>
2015-01-29 18:01:06 +0800

    work on project building and memory allocator

--- include/Utility/Memory/memory.h
+++ include/Utility/Memory/memory.h
@@ -1,8 +1,36 @@
-namespace Vense
+namespace Venus
 {
 	namespace Memory
 	{
-		void* Venus_new(int size);
+
+		class Allocator
+		{
+		public:
+			void *allocate();
+			void dellocate();
+
+		};
+
+		class DynamicPoolAllocator:public Allocator
+		{
+		private:
+			void *buffer;
+			unsigned int buffersize;
+		public:
+			DynamicPoolAllocator();
+			void* allocate();
+
+		};
+
+		template <class blocktype>
+		class BlockListAllocator:public Allocator
+		{ 
+			
+		};
+
+
+		void* Venus_new( int size, Allocator *allocator = 0);
 		void Venus_delete(void *pointer);
+		
 	}
 }
