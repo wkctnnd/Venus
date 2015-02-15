1d27b91f211ffb0cb92a56bd52a52f46cdc2ace5
wangkaichen <wkctnnd@sina.com>
2015-02-15 17:47:19 +0800

    add barely nothing

--- src/Resource/Image/imageloader.cpp
+++ src/Resource/Image/imageloader.cpp
@@ -5,50 +5,50 @@ namespace Venus
     namespace Resource
     {
 
-        class JpgImageLoader:public ImageLoader
+        class jpgImageLoader
         {
         public :
             static void LoadImageFile(Utility::VString &file);
         };
 
-        class ddsImageLoader:public ImageLoader
+        class ddsImageLoader
         {
         public:
             static void LoadImageFile(Utility::VString &file);
         };
 
 
-        class pngImageLoader:public ImageLoader
+        class pngImageLoader
         {
         public :
             static void LoadImageFile(Utility::VString &file);
         };
 
 
-             void ImageLoader:: loadImageFile(Utility::VString &file)
+        void ImageLoader:: loadImageFile(Utility::VString &file)
+        {
+            size_t pos = file.find(Utility::VString("."));
+            Utility::VString ext = file.subStr(pos, file.length() - pos);
+            if (ext.compare(Utility::VString("jpg")))
             {
-                size_t pos = file.find(Utility::VString("."));
-                Utility::VString ext = file.subStr(pos, file.length() - pos);
-                if (ext.compare(Utility::VString("jpg")))
-                {
-                    JpgImageLoader::loadImageFile(file);
-                }
-                else if(ext.compare(Utility::VString("png")))
-                {
-                
-                }
-
-
-                else if(ext.compare(Utility::VString("dds")))
-                {
-                
-                }
-
-                else
-                {
-                
-                }
+                jpgImageLoader::LoadImageFile(file);
             }
+            else if(ext.compare(Utility::VString("png")))
+            {
+                pngImageLoader::LoadImageFile(file);
+            }
+
+
+            else if(ext.compare(Utility::VString("dds")))
+            {
+                ddsImageLoader::LoadImageFile(file);
+            }
+
+            else
+            {
+
+            }
+        }
 
     }
 }
