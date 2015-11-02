c9d81209e915e5aa7d681cb537242b9b13e49094
wkctnnd <wkctnnd@sina.com>
2015-11-02 19:03:29 +0800

    unit test

---
+++ test/BaseTest.h
@@ -0,0 +1,22 @@
+#ifndef BASETEST_H
+#define BASETEST_H
+
+#include <functional>
+namespace Venus
+{
+    namespace Test
+	{
+		class TestInfo
+		{
+		public:
+			TestInfo()
+			{
+
+			}
+
+		public:
+			
+		};
+    }
+}
+#endif
